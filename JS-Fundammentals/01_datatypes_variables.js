90b027f29cb8db7d4a2eb42001d0608c615260cc
Arpita katara <arpitakatara2701@gmail.com>
2025-04-02 22:59:41 -0700

    Learned operators

---
+++ JS-Fundammentals/01_datatypes_variables.js
@@ -0,0 +1,35 @@
+//////////////////////////////////
+// 01. DATA TYPES
+/////////////////////////////////
+
+// 1.String , "Arpita", me, lastName);"19","true"
+// 2. Number ,19
+//3.BigIn
+// 4. Boolean , true / false
+// 5. Undefined
+// 6. Null
+// 7. Symobol
+
+
+/////////////////////////////
+// 02. vaeiables
+////////////////////////////
+// Identifiers/ Name 7 literals/Value
+var firstName = "Arpita";
+var lastName = "katara";
+var age = 19;
+var married = false;
+var husbsndname;  // undefined
+var thirdname = null;
+
+
+console.log(firstName, lastname);
+//print :''good morning Arpita katara /''
+console.log("good Morning", firstName, lastname,"|");  
+console.log("Hi, My name is" ,firstName,"and My age is",age, " ")
+console.log("husbandname")
+ 
+
+
+
+
